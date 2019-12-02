f6d6a1bbfb4b21970692e401773caa45f2383d61
Karen V <karenvo@usc.edu>
2019-12-02 10:50:08 +0000

    Automatically backed up by Learn

--- db/migrate/20191202104448_change_post_status_data_type_to_post.rb
+++ db/migrate/20191202104448_change_post_status_data_type_to_post.rb
@@ -1,4 +1,4 @@
-class ChangePostStatusDataTypeToPost < ActiveRecord::Migration[5.0]
+class ChangePostStatusDataTypeToPosts < ActiveRecord::Migration[5.0]
   def change
     change_column :posts, :posts_status, :string
   end
