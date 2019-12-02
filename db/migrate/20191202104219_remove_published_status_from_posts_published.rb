62b17462241e938d54d3845ba0335883b2233a9f
Karen V <karenvo@usc.edu>
2019-12-02 10:45:33 +0000

    Automatically backed up by Learn

--- db/migrate/20191202104219_remove_published_status_from_posts_published.rb
+++ db/migrate/20191202104219_remove_published_status_from_posts_published.rb
@@ -1,5 +1,5 @@
 class RemovePublishedStatusFromPostsPublished < ActiveRecord::Migration[5.0]
   def change
-    remove_column :posts_publisheds, :status_string, :string
+    remove_column :posts_published, :status_string, :string
   end
 end
